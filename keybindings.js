36e8e3c81d3f746fd4a70da63437d81819f42b2a
matt slaney <mdslaney@cisco.com>
2026-08-06 16:43:52 +0100

    Refine scratch layer shortcuts

--- keybindings.js
+++ keybindings.js
@@ -237,7 +237,7 @@ export function setupActions(settings) {
         (_mw, space) => space.swap(Meta.MotionDirection.DOWN));
 
     registerAction("toggle-scratch-window",
-        Scratch.toggleRecentScratchLayer,
+        Scratch.beginRecentScratchAction,
         { settings });
 
     registerAction("toggle-scratch-layer",
